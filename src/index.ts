63778a34ffe8bb5871123ffdd82e6551c397938a
Federico Moretti <dj.ikos90@gmail.com>
2019-03-17 17:32:41 +0100

    feat: inital reflow function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// track changes on dom
+// new MutationObserver()
+
 interface ReflowGridParameters {
   container: HTMLElement;
   containerWidth: number;
@@ -6,11 +9,44 @@ interface ReflowGridParameters {
 
 export default class ReflowGrid {
   container: HTMLElement;
+  children: HTMLElement[];
+  itemWidth: number;
+  containerWidth: number;
+  columnsCount: number;
+  columnsHeight: { [key: string]: number };
+
   constructor({ container, containerWidth, itemWidth }: ReflowGridParameters) {
     this.container = container;
+    this.children = Array.from(container.children) as HTMLElement[];
+    this.itemWidth = itemWidth;
+    this.containerWidth = containerWidth;
+    this.columnsCount = Math.floor(this.containerWidth / this.itemWidth);
+    this.columnsHeight = {};
     console.log('init');
 
+    this.initColumnsHeight();
+    console.log(this.columnsHeight);
+    this.addClassesToDOM();
     this.execute(containerWidth, itemWidth);
+  }
+
+  initColumnsHeight() {
+    Array.from(Array(this.columnsCount)).forEach((_, index) => {
+      this.columnsHeight[index + 1] = 0;
+    });
+  }
+
+  addClassesToDOM() {
+    const head = document.querySelector('head');
+    if (head) {
+      const style = document.createElement('style');
+      const css = document.createTextNode(`
+      /* reflow grid classes */
+      .rg-abs { position: absolute; }
+      `);
+      style.appendChild(css);
+      head.appendChild(style);
+    }
   }
 
   setWidth(element: HTMLElement, width: number) {
@@ -31,7 +67,43 @@ export default class ReflowGrid {
     console.log('execute!');
     this.setWidth(this.container, containerWidth);
     this.setChildrenWidth(this.container, itemWidth);
+    this.position();
   }
 
-  position() {}
+  getLowerColumn() {
+    const columns = Object.entries(this.columnsHeight);
+    return columns.reduce((prev, curr) => {
+      console.log(prev, curr);
+      if (curr[1] >= prev[1]) return prev;
+      return curr;
+    });
+  }
+
+  position() {
+    this.container.style.position = 'relative';
+    this.children.forEach((child, index) => {
+      child.classList.add('rg-abs');
+      let transform = `translateX(${index * this.itemWidth}px)`;
+      let column = index + 1;
+      console.log(index);
+
+      // check lower column
+      // then insert the child in the column
+      // the update column height
+      if (index * this.itemWidth >= this.containerWidth) {
+        console.log('+ 2nd row');
+        const lowerColumn = this.getLowerColumn();
+        column = Number.parseInt(lowerColumn[0]);
+        index = index - this.columnsCount;
+        const x = (column - 1) * this.itemWidth;
+        console.log('lowerColumn', lowerColumn);
+        transform = `translateX(${x}px) translateY(${lowerColumn[1]}px)`;
+      }
+
+      console.log(transform);
+      this.columnsHeight[column] += child.clientHeight;
+      console.log(this.columnsHeight);
+      child.style.transform = transform;
+    });
+  }
 }
--- umd/index.js
+++ umd/index.js
@@ -4,13 +4,38 @@
     (global = global || self, global.ReflowGrid = factory());
 }(this, function () { 'use strict';
 
+    // track changes on dom
+    // new MutationObserver()
     var ReflowGrid = /** @class */ (function () {
         function ReflowGrid(_a) {
             var container = _a.container, containerWidth = _a.containerWidth, itemWidth = _a.itemWidth;
             this.container = container;
+            this.children = Array.from(container.children);
+            this.itemWidth = itemWidth;
+            this.containerWidth = containerWidth;
+            this.columnsCount = Math.floor(this.containerWidth / this.itemWidth);
+            this.columnsHeight = {};
             console.log('init');
+            this.initColumnsHeight();
+            console.log(this.columnsHeight);
+            this.addClassesToDOM();
             this.execute(containerWidth, itemWidth);
         }
+        ReflowGrid.prototype.initColumnsHeight = function () {
+            var _this = this;
+            Array.from(Array(this.columnsCount)).forEach(function (_, index) {
+                _this.columnsHeight[index + 1] = 0;
+            });
+        };
+        ReflowGrid.prototype.addClassesToDOM = function () {
+            var head = document.querySelector('head');
+            if (head) {
+                var style = document.createElement('style');
+                var css = document.createTextNode("\n      /* reflow grid classes */\n      .rg-abs { position: absolute; }\n      ");
+                style.appendChild(css);
+                head.appendChild(style);
+            }
+        };
         ReflowGrid.prototype.setWidth = function (element, width) {
             console.log(width);
             element.style.width = width + "px";
@@ -28,8 +53,43 @@
             console.log('execute!');
             this.setWidth(this.container, containerWidth);
             this.setChildrenWidth(this.container, itemWidth);
+            this.position();
         };
-        ReflowGrid.prototype.position = function () { };
+        ReflowGrid.prototype.getLowerColumn = function () {
+            var columns = Object.entries(this.columnsHeight);
+            return columns.reduce(function (prev, curr) {
+                console.log(prev, curr);
+                if (curr[1] >= prev[1])
+                    return prev;
+                return curr;
+            });
+        };
+        ReflowGrid.prototype.position = function () {
+            var _this = this;
+            this.container.style.position = 'relative';
+            this.children.forEach(function (child, index) {
+                child.classList.add('rg-abs');
+                var transform = "translateX(" + index * _this.itemWidth + "px)";
+                var column = index + 1;
+                console.log(index);
+                // check lower column
+                // then insert the child in the column
+                // the update column height
+                if (index * _this.itemWidth >= _this.containerWidth) {
+                    console.log('+ 2nd row');
+                    var lowerColumn = _this.getLowerColumn();
+                    column = Number.parseInt(lowerColumn[0]);
+                    index = index - _this.columnsCount;
+                    var x = (column - 1) * _this.itemWidth;
+                    console.log('lowerColumn', lowerColumn);
+                    transform = "translateX(" + x + "px) translateY(" + lowerColumn[1] + "px)";
+                }
+                console.log(transform);
+                _this.columnsHeight[column] += child.clientHeight;
+                console.log(_this.columnsHeight);
+                child.style.transform = transform;
+            });
+        };
         return ReflowGrid;
     }());
 
